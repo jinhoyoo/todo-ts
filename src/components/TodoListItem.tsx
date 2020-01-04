529186827e7492519154732eb9cb0241e8d5ef97
Archer Yoo <brotherjinho@gmail.com>
2020-01-04 15:26:39 +0800

    finished styling work.

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,4 +1,10 @@
 import * as React from 'react';
+import {
+    MdCheckBoxOutlineBlank,
+    MdCheckBox,
+    MdRemoveCircleOutline,
+} from 'react-icons/md'
+import './TodoListItem.scss';
 
 export interface ITodoListItemProps {
 }
@@ -6,7 +12,13 @@ export interface ITodoListItemProps {
 export default function TodoListItem (props: ITodoListItemProps) {
   return (
     <div className="TodoListItem">
-      
+        <div className='checkbox'>
+            <MdCheckBox/>
+            <div className="text">"text"</div>
+        </div>
+        <div className="remove">
+            <MdRemoveCircleOutline/>
+        </div>
     </div>
   );
 }
